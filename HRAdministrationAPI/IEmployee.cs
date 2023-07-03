471035ae860f5c7030cdd824c14960745661d79f
AnF1ldzZ <nathakmon1902@gmail.com>
2023-07-03 15:33:56 +0700

    Add project files.

---
+++ HRAdministrationAPI/IEmployee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAdministrationAPI
+{
+    public interface IEmployee
+    {
+        int Id { get; set; }
+        string Firstname { get; set; }
+
+        string Lastname { get; set; }
+
+        decimal Salary { get; set; }
+    }
+}
